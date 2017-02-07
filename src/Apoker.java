765cc123d299fe86feea238b3b150f00b288c4e9
c16312 <c16312@172.16.3.120>
2017-02-07 14:25:00 +0900

    新規作成

--- src/Apoker.java
+++ src/Apoker.java
@@ -46,6 +46,8 @@ public class Apoker {
                 }             
             }
             
+            
+            
             humanPlayer.displayHand();
             
             for(int i=0; i<5; i++){
